cf3bdc242a91921cf027d8a6ce9495b0342f799d
Cristiano Ciuccarelli <cristianociuccarelli@gmail.comm>
2026-06-07 12:37:03 +0200

    Iterazione 2 - Controller: IUserController aggiornato con login

--- Hackaton/Iterazione2/src/main/java/it/unicam/Hackathon/controller/IUserController.java
+++ Hackaton/Iterazione2/src/main/java/it/unicam/Hackathon/controller/IUserController.java
@@ -1,6 +1,6 @@
 package it.unicam.Hackathon.controller;
 
-import it.unicam.Hackathon.dto.RegistrazioneRequest;
+import it.unicam.Hackathon.dto.*;
 import it.unicam.Hackathon.entity.Utente;
 import it.unicam.Hackathon.service.GestoreUtente;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +18,17 @@ public class IUserController {
     @PostMapping("/registrazione")
     public ResponseEntity<Map<String, Object>> registrazione(@RequestBody RegistrazioneRequest dto) {
         Utente u = gestoreUtente.registrazioneUtente(dto);
+        return ResponseEntity.ok(Map.of(
+                "messaggio", "Benvenuto " + u.getNome() + "!",
+                "id", u.getId(),
+                "utente", u.getNome() + " " + u.getCognome(),
+                "ruolo", u.getRuolo()
+        ));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest dto) {
+        Utente u = gestoreUtente.loginUtente(dto);
         return ResponseEntity.ok(Map.of(
                 "messaggio", "Benvenuto " + u.getNome() + "!",
                 "id", u.getId(),
