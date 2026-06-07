2ec6a50c66550e4dd60a27ee0f0457958cef2f46
Simone Pelosi <simonepelosi5@gmail.com>
2026-06-07 20:46:43 +0200

    Iterazione 3 - Controller: ISegnalazioneController aggiornato con sanziona

--- Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/controller/ISegnalazioneController.java
+++ Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/controller/ISegnalazioneController.java
@@ -1,7 +1,8 @@
 package it.unicam.Hackathon.controller;
 
-import it.unicam.Hackathon.dto.SegnalazioneRequest;
-import it.unicam.Hackathon.entity.Segnalazione;
+import it.unicam.Hackathon.dto.*;
+import it.unicam.Hackathon.entity.*;
+import it.unicam.Hackathon.service.GestoreHackathon;
 import it.unicam.Hackathon.service.GestoreSegnalazione;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,13 @@ import java.util.Map;
 public class ISegnalazioneController {
 
     private final GestoreSegnalazione gestoreSegnalazione;
+    private final GestoreHackathon gestoreHackathon;
 
     @PostMapping
     public ResponseEntity<Map<String, Object>> segnala(@RequestBody SegnalazioneRequest dto) {
         Segnalazione s = gestoreSegnalazione.inviaSegnalazione(dto);
         return ResponseEntity.ok(Map.of(
-                "messaggio", "Segnalazione registrata!",
-                "id", s.getId(),
+                "messaggio", "Segnalazione registrata!", "id", s.getId(),
                 "team", s.getTeam().getNome(),
                 "mentore", s.getMentore().getNome() + " " + s.getMentore().getCognome()
         ));
@@ -30,5 +31,15 @@ public class ISegnalazioneController {
     @GetMapping("/team/{teamId}")
     public ResponseEntity<List<Segnalazione>> visualizza(@PathVariable Long teamId) {
         return ResponseEntity.ok(gestoreSegnalazione.visualizzaSegnalazioni(teamId));
+    }
+
+    @PostMapping("/sanziona")
+    public ResponseEntity<Map<String, Object>> sanziona(@RequestBody SanzioneRequest dto) {
+        Segnalazione s = gestoreHackathon.applicaSanzione(dto);
+        return ResponseEntity.ok(Map.of(
+                "messaggio", "Sanzione Applicata!",
+                "id", s.getId(), "sanzione", s.getSanzione(),
+                "team", s.getTeam().getNome()
+        ));
     }
 }
