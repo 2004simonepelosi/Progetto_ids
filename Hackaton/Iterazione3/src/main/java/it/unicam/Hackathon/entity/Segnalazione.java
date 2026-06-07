f7d797ea524ccc4c9053e98d7edab5aa9a0f27e5
Ilaria Manara <ilaria.manara@studenti.unicam.it>
2026-06-07 18:30:41 +0200

    Iterazione 3 - Entity: Segnalazione aggiornata con organizzatore e sanzione

--- Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/entity/Segnalazione.java
+++ Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/entity/Segnalazione.java
@@ -1,8 +1,8 @@
 package it.unicam.Hackathon.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.;
-import lombok.;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "segnalazioni")
