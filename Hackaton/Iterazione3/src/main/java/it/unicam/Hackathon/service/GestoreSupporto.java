e0b01fb65f28e456203918492bde8802ed17730a
Cristiano Ciuccarelli <cristianociuccarelli@gmail.comm>
2026-06-07 19:13:05 +0200

    Iterazione 3 - Facade: GestoreSupporto

--- Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/service/GestoreSupporto.java
+++ Hackaton/Iterazione3/src/main/java/it/unicam/Hackathon/service/GestoreSupporto.java
@@ -1,2 +1,29 @@
-package it.unicam.Hackathon.service;public class GestoreSupporto {
+package it.unicam.Hackathon.service;
+
+import it.unicam.Hackathon.dto.SupportoRequest;
+import it.unicam.Hackathon.entity.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+/**
+ * Design Pattern: FACADE
+ */
+@Service
+@RequiredArgsConstructor
+public class GestoreSupporto {
+
+    private final SupportoService supportoService;
+    private final TeamService teamService;
+    private final HackathonService hackathonService;
+
+    public Supporto richiestaSupporto(SupportoRequest dto) {
+        Team team = teamService.findById(dto.getTeamId());
+        Hackathon hackathon = hackathonService.findById(dto.getHackathonId());
+        return supportoService.creaRichiesta(team, hackathon, dto.getDescrizione());
+    }
+
+    public List<Supporto> getRichieste(Long hackathonId) {
+        return supportoService.getRichieste(hackathonId);
+    }
 }
