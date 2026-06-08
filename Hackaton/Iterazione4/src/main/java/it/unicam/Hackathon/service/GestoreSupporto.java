d5b0673727c61ee4dbb41dc2c5e0ab3ebe04f213
Simone Pelosi <simonepelosi5@gmail.com>
2026-06-08 13:07:12 +0200

    Iterazione 4 - Facade: GestoreSupporto aggiornato con call

--- Hackaton/Iterazione4/src/main/java/it/unicam/Hackathon/service/GestoreSupporto.java
+++ Hackaton/Iterazione4/src/main/java/it/unicam/Hackathon/service/GestoreSupporto.java
@@ -1,14 +1,11 @@
 package it.unicam.Hackathon.service;
 
-import it.unicam.Hackathon.dto.SupportoRequest;
+import it.unicam.Hackathon.dto.*;
 import it.unicam.Hackathon.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-/**
- * Design Pattern: FACADE
- */
 @Service
 @RequiredArgsConstructor
 public class GestoreSupporto {
@@ -16,6 +13,7 @@ public class GestoreSupporto {
     private final SupportoService supportoService;
     private final TeamService teamService;
     private final HackathonService hackathonService;
+    private final UtenteService utenteService;
 
     public Supporto richiestaSupporto(SupportoRequest dto) {
         Team team = teamService.findById(dto.getTeamId());
@@ -25,5 +23,17 @@ public class GestoreSupporto {
 
     public List<Supporto> getRichieste(Long hackathonId) {
         return supportoService.getRichieste(hackathonId);
+    }
+
+    public CallSupporto proponeCall(ProponeCallRequest dto) {
+        Utente mentore = utenteService.findById(dto.getMentoreId());
+        Supporto supporto = supportoService.findById(dto.getSupportoId());
+        return supportoService.creaCallSupporto(
+                mentore, supporto, dto.getData(),
+                dto.getOra(), dto.getDurata(), dto.getNote());
+    }
+
+    public CallSupporto prenotaSlot(PrenotaSlotRequest dto) {
+        return supportoService.prenotaSlot(dto.getCallId(), dto.getSlotConfermato());
     }
 }
